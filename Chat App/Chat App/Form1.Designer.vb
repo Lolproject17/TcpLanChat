fb1bb59635a0f2845efb8984f51123b7bbed590e
Lolproject17 <32173070+Lolproject17@users.noreply.github.com>
2017-12-05 19:15:47 +0200

    Add files via upload

---
+++ Chat App/Chat App/Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 33)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(434, 277)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(95, 335)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(351, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 333)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Send"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(527, 333)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Send"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(610, 335)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(351, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Location = New System.Drawing.Point(527, 33)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(434, 277)
+        Me.RichTextBox2.TabIndex = 3
+        Me.RichTextBox2.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Server"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(973, 378)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Chat App/Chat App/Form1.vb
@@ -0,0 +1,105 @@
+﻿Imports System.Text
+Imports System.IO
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Threading.Thread
+Public Class Form1
+
+    Dim server As TcpListener = New TcpListener(IPAddress.Parse("192.168.1.3"), "13000")
+    Dim client As New TcpClient
+    Dim TcpClient As New TcpClient
+
+    Dim thread1 As New Threading.Thread(AddressOf ServerListen)
+    Dim thread3 As New Threading.Thread(AddressOf ClientListen)
+
+    Sub ClientListen()
+
+        TcpClient.Connect(IPAddress.Parse("192.168.1.3"), "13000")
+
+        Dim stream As NetworkStream = TcpClient.GetStream()
+
+        While True
+
+            If stream.DataAvailable = True Then
+                Dim array(TcpClient.ReceiveBufferSize) As Byte
+                Dim i As Integer
+                i = stream.Read(array, 0, array.Length)
+                RichTextBox2.Text = RichTextBox2.Text + "Him : " + System.Text.ASCIIEncoding.ASCII.GetString(array, 0, i) + vbNewLine
+                stream.Flush()
+            End If
+
+        End While
+
+
+
+    End Sub
+
+    Sub ClientAnswere()
+
+        Dim array() As Byte = System.Text.ASCIIEncoding.ASCII.GetBytes(TextBox2.Text)
+        TcpClient.Client.Send(array)
+        RichTextBox2.Text = RichTextBox2.Text + "Me : " + TextBox2.Text + vbNewLine
+        TextBox2.Text = ""
+
+    End Sub
+
+
+    Sub ServerListen()
+
+        server.Start()
+        client = server.AcceptTcpClient
+
+        Dim stream As NetworkStream = client.GetStream()
+
+        While True
+
+
+            If client.Available <> 0 Then
+
+
+                Dim Array(client.ReceiveBufferSize) As Byte
+                Dim i As Integer
+                Dim data As String
+                i = stream.Read(Array, 0, Array.Length)
+                data = System.Text.ASCIIEncoding.ASCII.GetString(Array, 0, i)
+                RichTextBox1.Text = RichTextBox1.Text + "Him : " + data + vbNewLine
+
+            End If
+
+        End While
+
+    End Sub
+
+    Sub serverWriteToClient()
+        Dim stream As NetworkStream = client.GetStream()
+
+        Dim array() As Byte = System.Text.ASCIIEncoding.ASCII.GetBytes(TextBox1.Text)
+        stream.Write(array, 0, array.Length)
+
+        RichTextBox1.Text = RichTextBox1.Text + "Me : " + TextBox1.Text + vbNewLine
+
+        TextBox1.Text = ""
+
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        thread1.Start()
+        thread3.Start()
+
+        System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = False
+
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim thread2 As New Threading.Thread(AddressOf serverWriteToClient)
+        thread2.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim thread4 As New Threading.Thread(AddressOf ClientAnswere)
+        thread4.Start()
+    End Sub
+End Class
